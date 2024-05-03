0b50f42c0ac391c486b1d5fbda52940a5835cb85
Willian Torrente <willian.nacional@gmail.com>
2024-05-03 17:41:53 -0300

    Feito os controlers  para Exceptions do Login

--- crm/src/main/java/com/saga/crm/controller/LoginController.java
+++ crm/src/main/java/com/saga/crm/controller/LoginController.java
@@ -1,37 +1,41 @@
 package com.saga.crm.controller;
 
-import com.saga.crm.model.Usuario;
-import com.saga.crm.repositories.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-
+import com.saga.crm.model.Usuario;
+import com.saga.crm.repositories.UsuarioRepository;
 
 @Controller
 public class LoginController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
-
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
-
     @GetMapping("/login")
     public String loginForm() {
         return "login";
     }
 
     @PostMapping("/login")
-    public String loginSubmit(@RequestParam String email, @RequestParam String senha) {
-        Usuario usuario = usuarioRepository.findByEmail(email);
-        if (usuario != null && bCryptPasswordEncoder.matches(senha, usuario.getSenha())) {
-            return "redirect:/dashboard";
+    public String loginSubmit(@RequestParam String email, @RequestParam String senha, RedirectAttributes attributes) {
+        try {
+            Usuario usuario = usuarioRepository.findByEmail(email);
+            if (usuario != null && bCryptPasswordEncoder.matches(senha, usuario.getSenha())) {
+                return "redirect:/dashboard";
+            }
+            attributes.addFlashAttribute("error", "Credenciais inválidas.\nFavor Insira novamente.");
+            return "redirect:/login";
+        } catch (Exception e) {
+            attributes.addFlashAttribute("error", "Erro ao tentar fazer login.\nColoque credenciais válidas.");
+            return "redirect:/login";
         }
-        return "redirect:/login";
     }
 
     @GetMapping("/cadastro")
@@ -40,17 +44,24 @@ public class LoginController {
     }
 
     @PostMapping("/cadastro")
-    public String cadastrarUsuario(@RequestParam String nome, @RequestParam String email, @RequestParam String senha) {
-        if (usuarioRepository.findByEmail(email) != null) {
+    public String cadastrarUsuario(@RequestParam String nome, @RequestParam String email, @RequestParam String senha,
+            RedirectAttributes attributes) {
+        try {
+            if (usuarioRepository.findByEmail(email) != null) {
+                attributes.addFlashAttribute("error", "Email já cadastrado.\nInsira outro email.");
+                return "redirect:/cadastro";
+            }
+
+            Usuario usuario = new Usuario();
+            usuario.setNome(nome);
+            usuario.setEmail(email);
+            usuario.setSenha(bCryptPasswordEncoder.encode(senha));
+            usuarioRepository.save(usuario);
+
+            return "redirect:/login";
+        } catch (Exception e) {
+            attributes.addFlashAttribute("error", "Erro ao tentar cadastrar usuário.\nColoque credenciais válidas.");
             return "redirect:/cadastro";
         }
-
-        Usuario usuario = new Usuario();
-        usuario.setNome(nome);
-        usuario.setEmail(email);
-        usuario.setSenha(bCryptPasswordEncoder.encode(senha));
-        usuarioRepository.save(usuario);
-
-        return "redirect:/login";
     }
 }
