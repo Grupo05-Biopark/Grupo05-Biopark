64a8224ebe7cc79baeea6bcef41d995d44eadeec
Willian Torrente <willian.nacional@gmail.com>
2024-05-26 14:29:49 -0300

    Finalizado a tela de expection do Login,Adicionado link de cadastro e esqueceu senha

--- crm/src/main/java/com/saga/crm/controller/LoginController.java
+++ crm/src/main/java/com/saga/crm/controller/LoginController.java
@@ -1,15 +1,20 @@
 package com.saga.crm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.saga.crm.model.Usuario;
 import com.saga.crm.repositories.UsuarioRepository;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class LoginController {
@@ -26,12 +31,17 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String loginSubmit(@RequestParam String email, @RequestParam String senha) {
+    @ResponseBody
+    public ResponseEntity<Map<String, String>> loginSubmit(@RequestParam String email, @RequestParam String senha) {
         Usuario usuario = usuarioRepository.findByEmail(email);
+        Map<String, String> response = new HashMap<>();
         if (usuario != null && bCryptPasswordEncoder.matches(senha, usuario.getSenha())) {
-            return "redirect:/dashboard";
+            response.put("message", "Login realizado com sucesso!");
+            return ResponseEntity.ok(response);
+        } else {
+            response.put("error", "Falha ao realizar login. Verifique suas credenciais e tente novamente.");
+            return ResponseEntity.status(401).body(response);
         }
-        return "redirect:/login";
     }
 
     @GetMapping("/cadastro")
@@ -43,6 +53,7 @@ public class LoginController {
     public String cadastrarUsuario(@RequestParam String nome, @RequestParam String email, @RequestParam String senha, RedirectAttributes attributes) {
         try {
             if (usuarioRepository.findByEmail(email) != null) {
+                attributes.addFlashAttribute("error", "Email já cadastrado.");
                 return "redirect:/cadastro";
             }
 
@@ -54,7 +65,7 @@ public class LoginController {
 
             return "redirect:/login";
         } catch (Exception e) {
-            attributes.addFlashAttribute("error", "Erro ao tentar cadastrar usuário.\nColoque credenciais válidas.");
+            attributes.addFlashAttribute("error", "Erro ao tentar cadastrar usuário. Coloque credenciais válidas.");
             return "redirect:/cadastro";
         }
     }
