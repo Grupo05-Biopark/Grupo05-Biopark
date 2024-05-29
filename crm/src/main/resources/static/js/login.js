f35f237eebb9581eb4334a97e9ab9d2f20f93ac1
Willian Torrente <willian.nacional@gmail.com>
2024-05-29 19:05:15 -0300

    commit

--- crm/src/main/resources/static/js/login.js
+++ crm/src/main/resources/static/js/login.js
@@ -1,95 +1,91 @@
+// Context: JavaScript file for 'login.html'
+    $(document).ready(function () {
+        $("#loginForm").on("submit", function (event) {
+            event.preventDefault();
 
-$(document).ready(function () {
-    $("#loginForm").on("submit", function (event) {
-      event.preventDefault();
+            var email = $("#email").val();
+            var senha = $("#senha").val();
 
-      var email = $("#email").val();
-      var senha = $("#senha").val();
+            function mostrarIndicadorDeCarregamento() {
+                $("#loadingIndicator").show();
+            }
 
-      // Função de validação dos campos e-mail e senha
-      function validarCampos(email, senha) {
-        // Vai Verifica se o campo de e-mail está preenchido e é válido
-        if (!email) {
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Por favor, preencha o campo de e-mail.",
-          });
-          return false;
-        }
+            function esconderIndicadorDeCarregamento() {
+                $("#loadingIndicator").hide();
+            }
 
-        // Vai Verifica se o e-mail tem um formato válido
-        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-        if (!emailRegex.test(email)) {
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Por favor, insira um e-mail válido.",
-          });
-          return false;
-        }
-        // Validar o e-mail com o  que esta cadastrado no banco de dados(Ainda não implementado)
-        if (email.length < 5) {
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "E-mail inválido. Por favor, insira um e-mail válido.",
-          });
-          return false;
-        }
-        // valida se a senha foi preenchida
-        if (!senha) {
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Por favor, preencha o campo de senha.",
-          });
-          return false;
-        }
-        // Validar com o banco de dados a senha(Ainda não implementado)
-        if (senha.length < 8) {
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "A senha deve ter pelo menos 8 caracteres.",
-          });
-          return false;
-        }
-        return true;
-      }
+            function validarCampos(email, senha) {
+                if (!email) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Por favor, preencha o campo de e-mail.",
+                    });
+                    return false;
+                }
 
-      var camposValidos = validarCampos(email, senha);
-      if (!camposValidos) {
-        return;
-      }
+                var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if (!emailRegex.test(email)) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Por favor, insira um e-mail válido.",
+                    });
+                    return false;
+                }
 
-      $.ajax({
-        url: "/login",
-        method: "POST",
-        data: {
-          email: email,
-          senha: senha,
-        },
-        success: function (response) {
-          Swal.fire({
-            icon: "success",
-            title: "Sucesso",
-            text: "Login realizado com sucesso!",
-            showConfirmButton: false,
-            timer: 2000,
-          }).then(function () {
-            window.location.href = "/dashboard";
-          });
-        },
-        error: function (xhr, status, error) {
-          Swal.fire({
-            icon: "error",
-            title: "Erro",
-            text:
-              xhr.responseJSON.error ||
-              "Falha ao realizar login. Verifique suas credenciais e tente novamente.",
-          });
-        },
-      });
+                if (!senha) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "Por favor, preencha o campo de senha.",
+                    });
+                    return false;
+                }
+
+                if (senha.length < 8) {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Oops...",
+                        text: "A senha deve ter pelo menos 8 caracteres.",
+                    });
+                    return false;
+                }
+
+                return true;
+            }
+
+            if (validarCampos(email, senha)) {
+                mostrarIndicadorDeCarregamento();
+
+                $.ajax({
+                    url: "/login",
+                    method: "POST",
+                    data: {
+                        email: email,
+                        senha: senha
+                    },
+                    success: function (response) {
+                        esconderIndicadorDeCarregamento();
+                        Swal.fire({
+                            icon: "success",
+                            title: "Sucesso",
+                            text: "Login realizado com sucesso!",
+                            showConfirmButton: false,
+                            timer: 2000,
+                        }).then(function () {
+                            window.location.href = "/dashboard";
+                        });
+                    },
+                    error: function (xhr, status, error) {
+                        esconderIndicadorDeCarregamento();
+                        Swal.fire({
+                            icon: "error",
+                            title: "Erro",
+                            text: xhr.responseJSON.error || "Falha ao realizar login. Verifique suas credenciais e tente novamente.",
+                        });
+                    }
+                });
+            }
+        });
     });
-  });
